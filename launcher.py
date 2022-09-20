121cfbd9b5c6691b494c2ddb53d6efa806848348
Rovnin Nikita <95008744+Emillionaire@users.noreply.github.com>
2022-09-20 18:16:47 +0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -10,6 +10,17 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 import db_application
 import vk_application
 
+
+def create_search_keyboard() -> dict:
+    """
+    Creates vk keyboard with search button
+    :return: keyboard json
+    """
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('search', color=VkKeyboardColor.SECONDARY)
+    return keyboard.get_keyboard()
+
+
 if __name__ == '__main__':
     # SETTINGS
     # read settings file
@@ -68,77 +79,88 @@ if __name__ == '__main__':
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
                 request = event.text
+
                 if request == 'привет':
                     write_msg(event.user_id,
                               'Привет, пользователь! Если хочешь начать поиск знакомств - напиши "начать".')
+
                 elif request == 'начать':
                     check = db_application.check_user_in_db(conn, event.user_id)
-                    if check is not None:
+                    if check is None:
                         write_msg(event.user_id, 'Сейчас я внесу тебя в свою базу данных!')
                         user_info = vk_application.get_user_info(user_id=event.user_id, VK_user_token=VK_user_token,
                                                                  URL_get=URL_get)
                         if user_info['city'] == 1:
                             write_msg(event.user_id,
-                                      'Я не смог определить твой город, извини. Буду искать, будто ты живешь в Москве. Если ты хочешь изменить свой город - напиши "изменить город"!')
+                                      'Я не смог определить твой город, извини. Буду искать, будто ты живешь в '
+                                      'Москве. Если ты хочешь изменить свой город - напиши "изменить город"!')
                         if user_info['bdate'] == 0:
                             write_msg(event.user_id,
-                                      'Я не смог определить твой год рождения, извини. Буду считать, будто ты родился в 2000 году. Если ты хочешь изменить год рождения - напиши "изменить год"!')
+                                      'Я не смог определить твой год рождения, извини. Буду считать, будто ты родился '
+                                      'в 2000 году. Если ты хочешь изменить год рождения - напиши "изменить год"!')
                         db_application.add_new_user(conn, user_info)
                         write_msg(event.user_id,
                                   'Чтобы запустить поиск подходящих для знакомтсва людей - напиши "искать"')
                     else:
                         write_msg(event.user_id, 'Я помню тебя. Давай продолжим работу. Напиши "смотреть"!')
+
                 elif request == 'искать':
                     write_msg(event.user_id, 'Начинаю подбор...')
                     vk_application.get_match_persons(conn, user_id=event.user_id, VK_user_token=VK_user_token,
                                                      URL_search=URL_search)
                     write_msg(event.user_id, '...подбор окончен! Напиши "смотреть", чтобы увидеть результаты')
+
                 elif request == 'смотреть':
                     count = 0
-                    clear_match_list = vk_application.get_persons_for_show(conn, event.user_id)
+                    clear_match_list = db_application.get_persons_for_show(conn, event.user_id)
                     if not clear_match_list:
-                        write_msg(event.user_id, 'У тебя закончились варианты или ты ещё не подбирал кандидатов. Напиши "искать", чтобы я составил новый список!')
-                        print(clear_match_list)
+                        write_msg(event.user_id, 'У тебя закончились варианты или ты ещё не подбирал кандидатов. '
+                                                 'Напиши "искать", чтобы я составил новый список!')
                     else:
                         result = vk_application.get_match_info(conn, clear_match_list[count], VK_user_token, URL_get)
                         watch_id = result["response"][0]["id"]
-                        try:
-                            take_photos = vk_application.get_photos(result["response"][0]["id"], VK_user_token,
-                                                                    URL_get_photo)
+                        take_photos = vk_application.get_photos(result["response"][0]["id"], VK_user_token,
+                                                                URL_get_photo)
+                        if take_photos is not None:
                             photos = []
                             for foto in take_photos[0:3]:
                                 photos.append(f'photo{foto[4]}_{foto[3]}_{VK_user_token}')
+                            photos = ','.join(photos)
                             write_msg_att(event.user_id,
                                           f'Имя: {result["response"][0]["first_name"]}'
                                           f'\nФамилия: {result["response"][0]["last_name"]}'
                                           f'\nСсылка: https://vk.com/id{result["response"][0]["id"]}'
-                                          f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы добавить человека в избранное',
+                                          f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы '
+                                          f'добавить человека в избранное',
                                           photos)
-                        except KeyError:
+                        else:
                             write_msg(event.user_id,
                                       f'Имя: {result["response"][0]["first_name"]}'
                                       f'\nФамилия: {result["response"][0]["last_name"]}'
                                       f'\nСсылка: https://vk.com/id{result["response"][0]["id"]}'
-                                      f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы добавить человека в избранное'
+                                      f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы добавить '
+                                      f'человека в избранное '
                                       f'\nЯ не смог загрузить фото...')
                         count += 1
                         db_application.add_reviewed_persons(conn, event.user_id, result['response'][0]['id'])
+
                 elif request == 'далее':
                     result = vk_application.get_match_info(conn, clear_match_list[count], VK_user_token, URL_get)
                     watch_id = result["response"][0]["id"]
-                    try:
-                        take_photos = vk_application.get_photos(result["response"][0]["id"], VK_user_token,
-                                                                URL_get_photo)
+                    take_photos = vk_application.get_photos(result["response"][0]["id"], VK_user_token,
+                                                            URL_get_photo)
+                    if take_photos is not None:
                         photos = []
                         for foto in take_photos[0:3]:
                             photos.append(f'photo{foto[4]}_{foto[3]}_{VK_user_token}')
+                        photos = ','.join(photos)
                         write_msg_att(event.user_id,
                                       f'Имя: {result["response"][0]["first_name"]}'
                                       f'\nФамилия: {result["response"][0]["last_name"]}'
                                       f'\nСсылка: https://vk.com/id{result["response"][0]["id"]}'
                                       f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы добавить человека в избранное',
                                       photos)
-                    except KeyError:
+                    else:
                         write_msg(event.user_id, f'Имя: {result["response"][0]["first_name"]}'
                                                  f'\nФамилия: {result["response"][0]["last_name"]}'
                                                  f'\nСсылка: https://vk.com/id{result["response"][0]["id"]}'
@@ -147,32 +169,35 @@ if __name__ == '__main__':
 
                     count += 1
                     db_application.add_reviewed_persons(conn, event.user_id, result['response'][0]['id'])
+
                 elif request == 'добавь':
                     db_application.add_fav_persons(conn, event.user_id, watch_id)
                     write_msg(event.user_id,
                               f'Добавил пользователя с ID {watch_id} в твой список избранных! Чтобы продолжить просмотр напиши "далее".')
+
                 elif request == 'избранное':
                     fav_list = db_application.take_user_favorite(conn, event.user_id)
                     for id in fav_list:
                         id_info = vk_application.get_favorite_info(id, VK_user_token, URL_get)
-                        try:
-                            take_photos = vk_application.get_photos(id_info["response"][0]["id"], VK_user_token,
-                                                                    URL_get_photo)
+                        take_photos = vk_application.get_photos(id_info["response"][0]["id"], VK_user_token,
+                                                                URL_get_photo)
+                        if take_photos is not None:
                             photos = []
                             for foto in take_photos[0:3]:
                                 photos.append(f'photo{foto[4]}_{foto[3]}_{VK_user_token}')
+                            photos = ','.join(photos)
                             write_msg_att(event.user_id,
                                           f'Имя: {id_info["response"][0]["first_name"]}'
                                           f'\nФамилия: {id_info["response"][0]["last_name"]}'
-                                          f'\nСсылка: https://vk.com/id{id_info["response"][0]["id"]}'
-                                          f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы добавить человека в избранное',
+                                          f'\nСсылка: https://vk.com/id{id_info["response"][0]["id"]}',
                                           photos)
-                        except KeyError:
+                        else:
                             write_msg(event.user_id, f'Имя: {id_info["response"][0]["first_name"]}'
                                                      f'\nФамилия: {id_info["response"][0]["last_name"]}'
                                                      f'\nСсылка: https://vk.com/id{id_info["response"][0]["id"]}'
-                                                     f'\nНапиши "далее" чтобы посмотреть другие варианты или "добавь" чтобы добавить человека в избранное'
                                                      f'\nЯ не смог загрузить его фото...')
+                    write_msg(event.user_id, f'Список избранных закончился, чтобы вернуться к просмотру кандидатов напиши "смотреть"!')
+
                 elif request == 'изменить город':
                     write_msg(event.user_id, 'Хорошо, в каком городе ты живешь? Напиши без ошибок и я попробую понять!')
                     want_change_city = 1
@@ -188,12 +213,13 @@ if __name__ == '__main__':
                     want_change_city = 0
 
                 else:
-                    try:
-                        request = int(request)
+                    if isinstance(request, int):
                         if request in range(1900, 2100) and want_change_year == 1:
                             db_application.change_user_bdate(conn, event.user_id, request)
                             write_msg(event.user_id, f'Отлично, я записал {request} как твой год рождения!')
                             want_change_year = 0
-                    except:
+                        else:
+                            pass
+                    else:
                         write_msg(event.user_id,
                                   'Не понял команду! Повтори предидущую команду или давай попробуем сначала - напиши "привет"!')
